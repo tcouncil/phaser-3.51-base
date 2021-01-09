6c411ec8149e47be646d127bcc0456922bf0a33e
Travis Council <travis@councilstudios.com>
2021-01-09 16:48:20 -0700

    added some basic text within our game scene

--- js/game.js
+++ js/game.js
@@ -1,6 +1,16 @@
 // create a new scene 
 let gameScene = new Phaser.Scene('Game');
 
+// load assets
+gameScene.preload = function () {
+
+};
+
+// called after the preload ends
+gameScene.create = function () {
+    this.add.text(240,20, `Let's make a game!`);
+};
+
 // config the game
 let config = {
     type: Phaser.AUTO, // Phaser will use WebGL if availabe, if not it will use Canvas for rendering
