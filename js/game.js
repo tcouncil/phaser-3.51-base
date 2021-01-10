6245088b1df38cb2aecd7d752d0499e77f88c465
Travis Council <travis@councilstudios.com>
2021-01-09 18:47:58 -0700

    added init() function

--- js/game.js
+++ js/game.js
@@ -1,17 +1,26 @@
 // create a new scene 
 let gameScene = new Phaser.Scene('Game');
 
+// initiate scene parameters
+gameScene.init = function(){
+    // Here we could declare player speed and other variables
+
+};
+
 // load assets
 gameScene.preload = function () {
+    // Here we could load sprites, audio and etc.
 
 };
 
 // called after the preload ends
 gameScene.create = function () {
+    // Here we can create our game objects
     this.add.text(240,20, `Let's make a game!`);
+
 };
 
-// our main gameloop in a sense (update() is called up to 60 times per second)
+// our main gameloop in a sense (update() is called up to 60 times per second) 60FPS sound familiar? 
 gameScene.update = function(){
 
 };
